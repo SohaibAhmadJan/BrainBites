40d2800edbf89a7975bc4a6471bcc75c9c1cefc0
Sohaib Ahmad Jan <7479163s@gmail.com>
2026-09-16 13:22:55 +0500

    Revert version to 4.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.brainbites"
         minSdk = 24
         targetSdk = 35
-        versionCode = 51
-        versionName = "4.4.8"
+        versionCode = 50
+        versionName = "4.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
